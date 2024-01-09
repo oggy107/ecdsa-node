1788ff057e2ef121e9adde379ec4b6b31cb87875
oggy107 <urmalsingh10@gmail.com>
2024-01-09 13:51:16 +0530

    updated: server

--- server/index.js
+++ server/index.js
@@ -1,44 +1,48 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3042;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+    "0x1": 100,
+    "0x2": 50,
+    "0x3": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
-  const { address } = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
+    const { address } = req.params;
+    const balance = balances[address] || 0;
+    res.send({ balance });
+});
+
+app.get("/accounts", (req, res) => {
+    res.send({ accounts: Object.keys(balances) });
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+    const { sender, recipient, amount } = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
-  }
+    if (balances[sender] < amount) {
+        res.status(400).send({ message: "Not enough funds!" });
+    } else {
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+        res.send({ balance: balances[sender] });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+    console.log(`Listening on port ${port}!`);
 });
 
 function setInitialBalance(address) {
-  if (!balances[address]) {
-    balances[address] = 0;
-  }
+    if (!balances[address]) {
+        balances[address] = 0;
+    }
 }
